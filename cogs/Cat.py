f4c6ee90aa026a3caa78589616dc682e3ab7f594
Kolby <winteryrs@gmail.com>
2022-08-24 18:10:56 -0600

    Cat Command Added Returns Random Cat

--- cogs/Cat.py
+++ cogs/Cat.py
@@ -8,11 +8,33 @@ from nextcord.ext.commands import has_permissions
 
 
 
+
+
+# affirmation = response.json()
+
+
+
+
 class Cat(commands.Cog):
     def __init__(self, client):
         self.client = client
+    
 
-    # Connect To Cat Image API
+    @commands.command(pass_context=True)
+    async def cat(self, ctx):
+        api_key = 'live_oQxlCVcGsl3KBfVTLdTrj5WT9Jtv0nf8aEbOjwxGK0l4tJwzqZMxLaWc779aexvX'
+        response = requests.get(f"https://api.thecatapi.com/v1/images/search?api_key={api_key}")
+        catobj = response.json()
+        catEmbed = nextcord.Embed(
+            title="Enjoy your Randomized Cat",
+            description=f"{catobj[0]['url']} \n\nRequested By <@{ctx.author.id}>",
+            url=f"",
+            color=0x2852fa
+            
+        )
+        catEmbed.set_image(url=f"{catobj[0]['url']}")
+        await ctx.send(embed=catEmbed)
+
 
 # Setup
 def setup(client):
